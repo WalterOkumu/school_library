ffceef86403a65c016be121934226077927cc5c7
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-11-06 23:09:06 +0300

    Completed Project 2 tasks

---
+++ person.rb
@@ -0,0 +1,28 @@
+require_relative './nameable'
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
