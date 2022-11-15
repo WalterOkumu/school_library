41bbe50f6c34e68ba5c2f3d28b7c133e94bb1eb9
skyv26 <dev.aakashv@gmail.com>
2022-11-15 23:02:38 +0530

    Resolve the linters errors

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 require_relative './menu_choice'
 
-$menu_choice = MenuChoice.new
+@menu_choice = MenuChoice.new
 
 def menu
   print <<~DOC
-  Please choose an option by entering a number:"
+    Please choose an option by entering a number:
     1 - List all books
     2 - List all people
     3 - Create a person
@@ -14,12 +14,12 @@ def menu
     7 - Exit App
   DOC
   print 'Enter you choice : '
-  choice = gets.chomp.to_i
+  gets.chomp.to_i
 end
 
 def prompt_user
   choice = menu
-  choice == 7 ? $menu_choice.exit_app : $menu_choice.option_case(choice)
+  choice == 7 ? @menu_choice.exit_app : @menu_choice.option_case(choice)
   prompt_user
 end
 
