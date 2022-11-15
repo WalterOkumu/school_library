2aae7084ace4cf1733d1895bc0ad5a6534f56e85
skyv26 <dev.aakashv@gmail.com>
2022-11-15 22:03:22 +0530

    reduced the space-complexity

--- main.rb
+++ main.rb
@@ -1,17 +1,16 @@
 require_relative './app'
 
 def menu
-  puts "\nPlease choose an option by entering a number:"
-  options = [
-    '1 - List all books',
-    '2 - List all people',
-    '3 - Create a person',
-    '4 - Create a book',
-    '5 - Create a rental',
-    '6 - List all rentals for a given person (id)',
-    '7 - Exit App'
-  ]
-  puts options
+  print <<~DOC
+  Please choose an option by entering a number:"
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - List all rentals for a given person (id)
+    7 - Exit App
+  DOC
 end
 
 def option_case(choice)
